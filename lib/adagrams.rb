8d7cd73327e7ee59a2bba9e049fb98b72da68343
Lindsay Terchin <lterchin@gmail.com>
2018-08-17 17:03:03 -0700

    returned winning score, handled ties

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -5,6 +5,7 @@ all_letters = []
 
 letters = {"A" => 9, "B" => 2, "C" => 2, "D"=> 4, "E" => 12, "F"=> 2, "G"=>3, "H"=>2, "I" => 9, "J"=>1, "K"=> 1, "L"=>4, "M"=> 2, "N"=> 6, "O"=>8, "P" => 2, "Q" => 1, "R"=>6, "S"=>4, "T"=>6, "U"=>4, "V"=>2, "W"=>2, "X"=>1, "Y"=>2, "Z"=>1}
 
+
 # Fill pool of letters array with letters
 letters.each do |letter, freq|
   count = 0
@@ -23,6 +24,7 @@ end
 
 DRAWN_LETTERS = draw_letters(all_letters)
 
+
 # Transform input word string into array, all in capital letters
 def make_word_array(input)
   input = input.upcase.split("")
@@ -30,10 +32,10 @@ def make_word_array(input)
 end
 
 
+#create method to see if word uses letters that are actually available
 def uses_available_letters? (input, drawn_letters)
   is_valid = "default"
   word_letters = make_word_array(input)
-
   word_letters.each do |letter|
     if drawn_letters.include?letter
       is_valid = true
@@ -44,28 +46,19 @@ def uses_available_letters? (input, drawn_letters)
       is_valid = false
       puts "letter not valid " + "#{is_valid}"
       return is_valid
-
     end
   end
-
   return is_valid
 end
 
 
 puts "drawn letters: #{DRAWN_LETTERS}"
 
-# LINES WORK  - test case with input
-# puts uses_available_letters?("LAME", DRAWN_LETTERS)
-
-# # LINES WORK - test case with user input
-# input = gets.chomp
-# puts uses_available_letters?(input, DRAWN_LETTERS)
-
 
 #turn all_letters array into a hash that represents the score of each letter
 LETTER_SCORES = {}
 
-# Make hash for letter to score - works
+# Make hash for letter to score
 all_letters.uniq.each do |letter|
 
   case letter
@@ -99,17 +92,13 @@ def score_word(word)
   word_letters.each do |letter|
     score += LETTER_SCORES[letter]
   end
-
   score += 8 if word.length >= 7 && word.length <= 10
   return score
 end
 
 
-# puts score_word("pillowsjamson")
-
-
 #create a method to return a hash with the highest scoring words
-def highest_score_from_words(words)
+def highest_score_from(words)
   all_words_scores = []
 
   # Calculate scores of all words
@@ -120,28 +109,28 @@ def highest_score_from_words(words)
     all_words_scores << each_word_score
   end
 
-###
-  # Find highest scoring word
-  # all_words_scores.sort_by do |word, score|
-  #   [:score]
-  # end
+  word_rank = all_words_scores.each.sort_by {|hash| hash[:score]}.reverse
 
-  # all_words_scores.each do |pair|
-  #    puts pair[:score]
-  # end
+  puts "word rank: #{word_rank}"
 
-  best_word = all_words_scores.each.sort_by {|hash| hash[:score]}.reverse.first
+  best_word = word_rank[0]
 
-# Need conditionals for ties
-#
-#
-#
-# binding.pry
- return best_word
+  word_rank.each_with_index do |hash, index|
 
-###
+    if word_rank[index][:score] > best_word[:score]
+      best_word = word_rank[index]
+
+    # Tie Handling
+    elsif word_rank[index][:score] == best_word[:score]
+      if best_word[:word].length != 10
+        if word_rank[index][:word].length == 10
+          best_word = word_rank[index]
+        elsif word_rank[index][:word].length < best_word[:word].length
+          best_word = word_rank[index]
+        end
+      end
+    end
+    index += 1
+  end
+  return best_word
 end
-
-# Test word array
-test_array = ["PUNKTASTIC", "JAM", "DOG", "PILLOW", "KAYAK"]
-puts "#{highest_score_from_words(test_array)}"
